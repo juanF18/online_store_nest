4f81dd1e4bb60841031331bc40c3bed7ea184813
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-06-23 23:59:43 -0500

    Se agrega el el metodo get para todos los usuarios, get para solo un usuario y el delete 1 usuario

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,13 +1,37 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CrearUsuarioDTO } from './dto/crear-usuario.dto';
 import { UsuariosService } from './usuarios.service';
+import { Usuario } from './usuario.entity';
 
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private servicioUsuario: UsuariosService) {}
 
   @Post()
-  crearUsuario(@Body() nuevoUsuario: CrearUsuarioDTO) {
+  crearUsuario(@Body() nuevoUsuario: CrearUsuarioDTO): Promise<Usuario> {
     return this.servicioUsuario.crearUsuario(nuevoUsuario);
+  }
+
+  @Get()
+  obtenerUsuarios(): Promise<Usuario[]> {
+    return this.servicioUsuario.obtenerUsuarios();
+  }
+
+  @Get(':id')
+  obtenerUsuario(@Param('id', ParseIntPipe) id: number): Promise<Usuario> {
+    return this.servicioUsuario.obtenerUsuario(id);
+  }
+
+  @Delete(':id')
+  eliminarUsuario(@Param('id', ParseIntPipe) id: number) {
+    this.servicioUsuario.eliminarUsuario(id);
   }
 }
