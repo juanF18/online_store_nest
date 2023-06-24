b67e97b83af3423df7303d1fe0c746d99cbe809f
Juan Felipe <juanfelipecortescastrillon@gmail.com>
2023-06-23 20:47:17 -0500

    Add initial config of data base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'dotenv/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsuariosModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DEV_HOST,
+      port: Number(process.env.DEV_PORT),
+      username: process.env.DEV_USER,
+      password: process.env.DEV_PASS,
+      database: process.env.DEV_DB_NAME,
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+      logging: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
